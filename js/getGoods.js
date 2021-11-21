8fcb69af5cd939d357bf6940f9b1bdc0f52b1ca1
Mishgun666 <mishgun_1988@mail.ru>
2021-11-21 22:58:24 +0300

    third lesson

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,11 +1,13 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link')
 
-    const getData = () => {
+    const getData = (value, category) => {
         fetch('/db/db.json')
             .then((res) => res.json())
             .then((data) => {
-                localStorage.setItem('data', JSON.stringify(data))
+                const array = category ? data.filter((item) => item[category] === value) : data
+
+                localStorage.setItem('goods', JSON.stringify(array))
             })
     }
 
@@ -14,7 +16,11 @@ const getGoods = () => {
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
             event.preventDefault()
-            getData()
+            const linkValue = link.textContent
+            const category = link.dataset.field
+
+
+            getData(linkValue, category)
         });
     })
 }
